bf66d59320331390a077d9382d985a9ff65ab488
iagol lima <iagol360@gmail.com>
2023-09-16 14:21:29 -0300

    sync

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
+using System.Collections.Generic;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int>numbers = new List<int>();
+        int number= -1;
+        int sum = 0;
+        while(number != 0)
+       {
+            Console.WriteLine("Enter number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+        
+            if(number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is : {average}");
+
+        int max = numbers[0];
+
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+}
 }
