82c7588069f72560d857098c7f0dd89674b66a08
iagol lima <iagol360@gmail.com>
2023-09-16 13:36:12 -0300

    changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string InputScore = Console.ReadLine();
+        int score = int.Parse(InputScore);
+        string letter = ("");
+
+        if (score >= 90)
+        {
+            letter = ("A");
+        }
+
+        else if (score<90 && score >=80)
+        {
+            letter = ("B");
+        }
+
+        else if (score<80 && score>=70)
+        {
+            letter = ("C");
+        }
+
+        else if (score<70 && score>=60)
+        {
+            letter = ("D");
+        }
+
+        else
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine(letter);
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed in this course, you did very well.");
+        }
+        else
+        {
+            Console.WriteLine("It wasn't that time but don't be sad you can try again, the secret its to not give up");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+
+        string response = "";
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0,100);
+            int guess = -1;
+            int count = 0;
+
+            while(guess != number)
+            {
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it");
+                    Console.WriteLine($"You tried {count} times");
+                    Console.WriteLine("Do you want to play again?");
+                    response = Console.ReadLine();
+                }
+                count ++;
+            }
+        }
+        while(response == "yes");
     }
 }
