cb83c627742ee96f0768aba303d015b47e76c5e0
iagol lima <iagol360@gmail.com>
2023-09-21 20:54:53 -0300

    sync

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
-
-class Program
+    
+    class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Administrative assistant";
+        job1._company = "SOMOS";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Office assistant";
+        job2._company = "Speedcar";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Iago Lima";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
